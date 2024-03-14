f61be0a8eba7a3362b2bef736117be64d61d12ff
nanditha-intel <93230708+nanditha-intel@users.noreply.github.com>
2024-03-13 23:58:52 -0700

    Update: moving Quartus versions (#1332)
    * updating Quartus versions
    
    * revert S10 files
    
    * revert more S10 files
    
    * revert d5005 and f200x back to 23.4
    
    * [Fix] Avoid specifying a build number for bundle/regtest/24.1
    
    * updating header on n6001 and common files
    
    * edit Jenkins to disable the build_adp for now till HSD 16023514565 is fixed in final quartus build
    
    ---------
    
    Co-authored-by: Chip Bilbrey <chip.bilbrey@intel.com>

--- src/common/he_hssi/pkt_client_mac_seg/nbq_scsdpram.sv
+++ src/common/he_hssi/pkt_client_mac_seg/nbq_scsdpram.sv
@@ -2,29 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 
-//-----------------------------------------------------------------------------
-// (C) 2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions and other 
-// software and tools, and its AMPP partner logic functions, and any output 
-// files from any of the foregoing (including device programming or simulation 
-// files), and any associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License Subscription 
-// Agreement, Altera MegaCore Function License Agreement, or other applicable 
-// license agreement, including, without limitation, that your use is for the 
-// sole purpose of programming logic devices manufactured by Altera and sold by 
-// Altera or its authorized distributors.  Please refer to the applicable 
-// agreement for further details.
-//-----------------------------------------------------------------------------
-
-//-----------------------------------------------------------------------------
-// Revision history:-
-//
-// 18/May/2015  | Creation.
-//              | 
-//              | Single clock simple-dual-port mode RAM. This is a 
-//              | parameterised version of a Megawizard generated wrapper.
-//-----------------------------------------------------------------------------
-
 module nbq_scsdpram # (
    parameter INIT_FILE       = "packet_client.hex",
    parameter SKIP_RDEN   	  = 0,
